85b773da03bc8d17ccf7c6b835b25e4381d87ee2
cclauss <cclauss@bluewin.ch>
2017-07-12 08:09:01 +0200

    Update trending_python_tester.py

--- trending_python_tester.py
+++ trending_python_tester.py
@@ -32,9 +32,9 @@ script:
     - git clone --depth=50 --branch=master ${URL} ~/${REPO}
     - cd ~/${REPO}
     - echo ; echo -n "flake8 testing of ${URL} on " ; python -V
-    # stop the build if there are Python syntax errors or undefined names
+    - echo stop the build if there are Python syntax errors or undefined names
     - flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics
-    # exit-zero treats all errors as warnings.  The GitHub editor is 127 chars wide
+    - echo exit-zero treats all errors as warnings.  The GitHub editor is 127 chars wide
     - flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 notifications:
     on_success: change
