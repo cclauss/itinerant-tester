06cbcf1f6eadad81dff8321e3649eceb8e8834b6
Christian Clauss <cclauss@me.com>
2020-01-09 12:48:57 +0100

    Update trending_python_tester.py

--- trending_python_tester.py
+++ trending_python_tester.py
@@ -16,9 +16,21 @@ import bs4  # will require lxml or html5lib
 import requests
 from github3 import login as github3_login
 
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:60.0)"
+    " Gecko/20100101 Firefox/60.0"
+}
+
+token = "cf2800ef2802b619a8ca8bf360f68ee4547a38c3"
+
+
+def my_two_factor_function():
+    return input("Enter 2FA code: ").strip() or my_two_factor_function()
+
+
 username = getpass.getuser()  # Does local username == GitHub username?
 print("Please enter the GitHub password for user: {}".format(username))
-gh = github3_login(username, getpass.getpass())
+gh = github3_login(username, token, two_factor_callback=my_two_factor_function)
 
 # url = 'https://github.com/trending/jupyter-notebook'  # GitHub Trending top 25 repos
 url = "https://github.com/trending/python"  # GitHub Trending top 25 repos
@@ -30,15 +42,15 @@ ignore = []
 
 # the boilerplate content of the .travis.yml file
 fmt = """group: travis_latest
-dist: xenial  # required for Python >= 3.7 (travis-ci/travis-ci#9069)
+dist: bionic
 language: python
 cache: pip
 python:
   #- 2.7
-  #- 3.4
   #- 3.5
   #- 3.6
-  - 3.7
+  #- 3.7
+  - 3.8
 matrix:
   allow_failures:
     - python: 2.7
@@ -58,7 +70,7 @@ before_script:
 script:
   - echo stop the build if there are Python syntax errors or undefined names
   - echo ; echo -n "flake8 testing of ${URL} on " ; python -V
-  - time flake8 . --count --select=E901,E999,F821,F822,F823 --show-source --statistics
+  - time flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
   - echo exit-zero treats all errors as warnings.  The GitHub editor is 127 chars wide
   - time flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
   #- true  # pytest --capture=sys
@@ -69,43 +81,53 @@ notifications:
 
 print(f"{dt.now():%a %b %d %H:%M:%S %Z %Y}")
 # extract the repo names of GitHub's Top 25 Trending Python list
-soup = bs4.BeautifulSoup(requests.get(url).content, "lxml")  # or 'html5lib'
+soup = bs4.BeautifulSoup(
+    requests.get(url, headers=HEADERS).content, "lxml"
+)  # or 'html5lib'
 # 'python / cpython'
-repos = soup.find("ol", class_="repo-list").find_all("a", href=True)
+# repos = soup.find('ol', class_="repo-list").find_all('a', href=True)
+repos = [h3.find("a", href=True) for h3 in soup.find_all(class_="h3 lh-condensed")]
+print(repos)
 # 'python/cpython'
 repos = (
-    repo.text.strip().replace(" ", "")
+    repo.text.strip().replace(" ", "").replace("\n" * 4, "")
     for repo in repos
     if "/" in repo.text and "://" not in repo.text
 )
 repos = list(repos) + [
+    "n1nj4sec/pupy",
     "PythonCharmers/python-future",
-    # 'ansible/awx', 'cheshirekow/cmake_format', 'n1nj4sec/pupy',
-    # 'ArduPilot/pymavlink', 'dronekit/dronekit-python',
     "ansible/ansible",
-    "Tribler/tribler",
     "oaubert/python-vlc",
     "ColdGrub1384/Pyto",
-    "getsentry/sentry",
     "CoreSecurity/impacket",
-    "nodejs/node",
-    "nodejs/node-gyp",
     "internetarchive/openlibrary",
     "google/ffn",
-    "webpy/webpy",
-    "ibm-watson-iot/connector-cloudant",
-    "ibm-watson-iot/device-kodi",
-    "ArduPilot/ardupilot",
-    "matplotlib/matplotlib",
-    "ckan/ckan",
-    "ggtracker/sc2reader",
     "apache/beam",
     "apache/incubator-mxnet",
-    "apache/spark",
+    "hyperledger/iroha-python",
+    "Supervisor/supervisor",
+    "cheshirekow/cmake_format",
+    "ckan/ckan",
+    "ibm-watson-iot/connector-cloudant",
+    "ibm-watson-iot/device-kodi",
     "ibm-watson-iot/functions",
+    "apache/spark",
+    "Tribler/tribler",
+    "webpy/webpy",
+    "nodejs/node",
+    "nodejs/node-gyp",
+    "ArduPilot/ardupilot",
+    "ArduPilot/pymavlink",
+    "dronekit/dronekit-python",
+    "ansible/awx",
+    "matplotlib/matplotlib",
+    "ggtracker/sc2reader",
+    "httplib2/httplib2",
+    "hyperledger/fabric-sdk-py",
+    "getsentry/sentry",
+    "v8/v8",
 ]
-# 'httplib2/httplib2', 'Supervisor/supervisor'
-# 'hyperledger/fabric-sdk-py', 'hyperledger/iroha-python']
 # '    - REPO=python/cpython'  also strip out any repos that are in ignore list
 repos = "\n".join("  - REPO=" + repo for repo in repos)
 #                 if 'shadowsocks' not in repo and repo not in ignore)
@@ -115,4 +137,4 @@ travis_text = fmt % repos
 # log into GitHub and commit an update to .travis.yml which will trigger tests
 travis = gh.repository(username, "itinerant-tester").file_contents("/.travis.yml")
 print(travis.update("trigger a new build", travis_text.encode("utf-8")))
-webbrowser.open("https://travis-ci.org/{}/itinerant-tester".format(username))
+webbrowser.open("https://travis-ci.com/{}/itinerant-tester".format(username))
