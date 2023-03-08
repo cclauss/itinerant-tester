31c14ed5c295e8562ad4c242127f4147e8d756e1
Christian Clauss <cclauss@me.com>
2023-03-09 00:14:49 +0100

    Update ruff_generate_baseline.py

--- ruff_generate_baseline.py
+++ ruff_generate_baseline.py
@@ -4,13 +4,13 @@ from __future__ import annotations
 from subprocess import run  # Requires Python >= 3.7
 
 ruff_out: tuple[str] = tuple(
-    run(
+    line for line in run(
         ["ruff", "--extend-select=C90", "."], capture_output=True, text=True
-    ).stdout.splitlines()
+    ).stdout.splitlines() if not line.startswith("warning:")
 )
 
 
-def get_max_complexity(ruff_out: Tuple[str] = ruff_out) -> int:
+def get_max_complexity(ruff_out: tuple[str] = ruff_out) -> int:
     C901_TAG = " is too complex ("
     c901_lines = [line for line in ruff_out if "C901" in line and C901_TAG in line]
     if not c901_lines:
@@ -18,7 +18,7 @@ def get_max_complexity(ruff_out: Tuple[str] = ruff_out) -> int:
     return max(int(line.split(C901_TAG)[-1].split(")")[0]) for line in c901_lines)
 
 
-def get_max_line_length(ruff_out: Tuple[str] = ruff_out) -> int:
+def get_max_line_length(ruff_out: tuple[str] = ruff_out) -> int:
     E501_TAG = ": E501 line too long ("
     e501_lines = [line for line in ruff_out if E501_TAG in line]
     if not e501_lines:
