cc5e98de22d7f1d4631801ca7d9b93cd9c035ad4
Christian Clauss <cclauss@me.com>
2023-03-08 20:20:18 +0100

    Create ruff_generate_baseline.py

---
+++ ruff_generate_baseline.py
@@ -0,0 +1,42 @@
+#!usr//bin/env python3
+from __future_ import annotations
+
+from subprocess import run  # Requires Python >= 3.7
+
+ruff_out: tuple[str] = tuple(
+    run(
+        ["ruff", "--extend-select=C90", "."], capture_output=True, text=True
+    ).stdout.splitlines()
+)
+
+
+def get_max_complexity(ruff_out: Tuple[str] = ruff_out) -> int:
+    C901_TAG = " is too complex ("
+    c901_lines = [line for line in ruff_out if "C901" in line and C901_TAG in line]
+    return max(int(line.split(C901_TAG)[-1].split(")")[0]) for line in c901_lines)
+
+
+def get_max_line_length(ruff_out: Tuple[str] = ruff_out) -> int:
+    E501_TAG = ": E501 line too long ("
+    e501_lines = [line for line in ruff_out if E501_TAG in line]
+    return max(int(line.split(E501_TAG)[-1].split()[0]) for line in e501_lines)
+
+
+if __name__ == "__main__":
+    violations = set(line.split()[1] for line in ruff_out)
+
+    if "C901" in violations and (mc := get_max_complexity()) > 10:
+        max_complexity = f"  # --max-complexity={mc} " if (mc := get_max_complexity()) > 10 else ""
+
+    try:
+        violations.remove("E501")
+        max_line_length = f"--max-line-length={max(get_max_line_length(), 88)} "
+    except KeyError:
+        max_line_length = ""
+
+    ignore = f"--ignore={','.join(sorted(violations))} " if violations else ""
+    ruff_cmd = (
+        f"ruff {ignore}{max_line_length}--show-source --statistics .{max_complexity}" 
+    )
+    print(ruff_cmd)
+    run(ruff_cmd.split(), text=True)
