17c76c539725d297dd2b14fe173c325dee55b968
Christian Clauss <cclauss@me.com>
2023-03-23 14:00:07 +0100

    Add scripts/ruff_config_gen.py (#4)

---
+++ scripts/ruff_config_gen.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Generate ruff TOML config from ruff output."""
+
+from __future__ import annotations
+
+
+def ruff_config_gen(lines: list[str]) -> None:
+    """Generate ruff TOML config from ruff output.
+
+    ex. ruff --select=ALL . | ./ruff_config_gen.py
+    line-length = 148
+    max-branches = 21
+    max-statements = 60
+    """
+    rules = {
+        "E501": "line-length",
+        "PLR0913": "max-args",
+        "PLR0912": "max-branches",
+        "PLR0911": "max-returns",
+        "PLR0915": "max-statements",
+    }
+    for rule, config in rules.items():
+        if maximum := max(
+            (int(_.split("(")[-1].split()[0]) for _ in lines if _.split()[1] == rule),
+            default=0,
+        ):
+            print(f"{config} = {maximum}")  # noqa: T201
+
+
+if __name__ == "__main__":
+    from sys import stdin
+
+    ruff_config_gen(lines=stdin.readlines()[:-1])
