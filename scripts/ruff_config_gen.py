abc2749eb43009c7c4e6c82312cd4ec189484361
Christian Clauss <cclauss@me.com>
2023-03-24 10:12:35 +0100

    Update ruff_config_gen.py

--- scripts/ruff_config_gen.py
+++ scripts/ruff_config_gen.py
@@ -3,6 +3,19 @@
 
 from __future__ import annotations
 
+ruff_header = """
+[tool.ruff]
+extend-select = [
+    "C9",
+    "PLR091",
+    "S",
+]
+target-version = py37
+"""
+
+ruff_pylint_header = """
+[tool.ruff.pylint]"""
+
 
 def ruff_config_gen(lines: list[str]) -> None:
     """Generate ruff TOML config from ruff output.
@@ -20,7 +33,7 @@ def ruff_config_gen(lines: list[str]) -> None:
         "PLR0911": "max-returns",
         "PLR0915": "max-statements",
     }
-    print("\n[tool.ruff]")
+    print(ruff_header)
     need_pylint_header = True
     for rule, config in rules.items():
         if maximum := max(
@@ -28,7 +41,7 @@ def ruff_config_gen(lines: list[str]) -> None:
             default=0,
         ):
             if need_pylint_header and rule.startswith("PL"):
-                print("\n[tool.ruff.pylint]")
+                print(ruff_pylint_header)
                 need_pylint_header = False
             print(f"{config} = {maximum}")  # noqa: T201
 
