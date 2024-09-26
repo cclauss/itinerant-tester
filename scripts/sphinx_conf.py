cce5802f917771870c3b21ed257374b87dc4060f
Christian Clauss <cclauss@me.com>
2024-09-26 18:59:08 +0200

    Create scripts/sphinx_conf.py

---
+++ scripts/sphinx_conf.py
@@ -0,0 +1,63 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "itinerant_sphinx_autodoc2.yml"
+version = "0.0.1"
+copyright = "2024, You"
+author = "cclauss/itinerant-tester"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+]
+extensions = [
+    "autodoc2",
+    "myst_parser",
+]
+# autodoc2_output_dir = "docs/_build/html"
+#autodoc2_packages = [
+#    "..",
+#]
+autodoc2_render_plugin = "myst"
+myst_enable_extensions = [
+    "amsmath",
+    "attrs_inline",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    # "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
+myst_fence_as_directive = [
+    "include",
+    "mermaid",
+]
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".txt": "markdown",
+    ".md": "markdown",
+}
+suppress_warnings = ["epub.unknown_project_files"]
+templates_path = ["_templates"]
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_static_path = ["_static"]
+# html_theme = "alabaster"
